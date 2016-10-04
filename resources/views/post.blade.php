880f5fd3f8b0d889b042175bbc12463ef4da31c7
codehimanshu <himanshuagrawal1998@gmail.com>
2016-10-04 14:40:53 +0530

    minor fixes in post view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+	@extends('layouts.app')
 
 @section('js')
 	<meta property="og:url"           content="http://localhost:8000/post/{{$post['id']}}" />
@@ -9,18 +9,15 @@
 <div class="container">
 	<div class="row header">
 		<!-- ID: {{ $post['id'] }}<br> -->
-		<div class="title">
+		<div id="title" class="col-xs-12 col-md-6 col-lg-6">
 			{{ $post['title'] }} 
 		</div>
-		<div class="author">
+		<div id="author" class="col-xs-9 col-md-4 col-lg-4">
 			&nbsp; by {{ $post['author'] }}
 		</div>
-		<span style="visibility: hidden;">
-			{{ $id=$post['id'] }}
-		</span>
-		<div class="like">
+		<div id="like" class="col-xs-3 col-md-2 col-lg-2">
 			<div class="fb-like" 
-				data-href="http://techblog.pagekite.me/post/{{$id}}" 
+				data-href="http://techblog.pagekite.me/post/{{$post['id']}}" 
 				data-layout="button_count" 
 				data-action="like" 
 				data-show-faces="true">
@@ -32,4 +29,8 @@
 	</div>
 </div>
 
+@endsection
+
+@section('script')
+    <script type="text/javascript" src="http://localhost/tech_blog/resources/assets/js/post.js"></script>
 @endsection
