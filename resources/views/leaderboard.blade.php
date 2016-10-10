91c2f265e5829af00b6404a8fb32ba307156260d
Milinda Laknath Arambawela <MilindaLaknath@users.noreply.github.com>
2016-10-10 16:59:45 +0530

    Browser Agent Added in CURL request
    Browser Agent Added in CURL request in leaderboard.blade.php
    Check this working. It worked when I checked it.

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -8,6 +8,14 @@
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	
+	// Browser Agent Added
+	curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.52 Safari/537.17');
+    	curl_setopt($ch, CURLOPT_AUTOREFERER, true); 
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    	curl_setopt($ch, CURLOPT_VERBOSE, 1);
+	
 	$data = curl_exec($ch);
 	curl_close($ch);
 	var_dump($data);
